b5cfdb057aeaa4c62193607b2e748de261521d8e
revaldidwioctavian <revaldidwioctavian@gmail.com>
2023-07-12 10:36:00 +0700

    Create RombelManagementController

---
+++ app/Http/Controllers/API/Admin/RombelManagement/RombelManagementController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin\RombelManagement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+
+class RombelManagementController extends Controller
+{
+    
+}
