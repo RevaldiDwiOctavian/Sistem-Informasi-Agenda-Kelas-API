b0f5a9effaeb10f67403758ff5c2068edfb86f9b
revaldidwioctavian <revaldidwioctavian@gmail.com>
2023-07-12 14:41:24 +0700

    create file AgendaKelasManagementController

---
+++ app/Http/Controllers/API/Admin/AgendaKelasManagement/AgendaKelasManagementController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin\AgendaKelasManagement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AgendaKelasManagementController extends Controller
+{
+    //
+}
