44e60bd7cb3f4e938a308777dbb97ea7a363b82d
Revaldi Dwi Octavian <revaldidwioctaviann@gmail.com>
2023-08-21 16:14:33 +0700

    Cors to all

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sistem-informasi-agenda-kelas.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
